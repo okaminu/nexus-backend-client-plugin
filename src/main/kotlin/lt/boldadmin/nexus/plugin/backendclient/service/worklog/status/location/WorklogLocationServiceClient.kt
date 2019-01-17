859273327e664c5b0c7d97112b8170cb9bbfc58d
Okaminu <aurimasdgt@gmail.com>
2019-01-17 17:45:54 +0200

    BA-742: sektor-backend uses nexus-backend-client-plugin to access nexus

---
+++ src/main/kotlin/lt/boldadmin/nexus/plugin/backendclient/service/worklog/status/location/WorklogLocationServiceClient.kt
@@ -0,0 +1,11 @@
+package lt.boldadmin.nexus.plugin.backendclient.service.worklog.status.location
+
+import lt.boldadmin.nexus.api.service.worklog.status.location.WorklogLocationService
+import lt.boldadmin.nexus.api.type.entity.Collaborator
+import lt.boldadmin.nexus.api.type.valueobject.Location
+
+class WorklogLocationServiceClient: WorklogLocationService {
+    override fun logWork(collaborator: Collaborator, collaboratorLocation: Location) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ src/main/kotlin/lt/boldadmin/nexus/plugin/backendclient/service/worklog/status/message/WorklogMessageServiceClient.kt
@@ -0,0 +1,10 @@
+package lt.boldadmin.nexus.plugin.backendclient.service.worklog.status.message
+
+import lt.boldadmin.nexus.api.service.worklog.status.message.WorklogMessageService
+import lt.boldadmin.nexus.api.type.valueobject.Message
+
+class WorklogMessageServiceClient: WorklogMessageService {
+    override fun logWork(message: Message) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
