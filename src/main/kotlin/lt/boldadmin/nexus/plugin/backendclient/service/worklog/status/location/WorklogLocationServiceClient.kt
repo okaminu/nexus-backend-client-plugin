6ba8c9d840810b07094de93e1ad1355f4c70b4fa
Okaminu <aurimasdgt@gmail.com>
2019-01-17 18:03:59 +0200

    BA-742: sektor-backend uses nexus-backend-client-plugin to access nexus

--- src/main/kotlin/lt/boldadmin/nexus/plugin/backendclient/service/worklog/status/location/WorklogLocationServiceClient.kt
+++ src/main/kotlin/lt/boldadmin/nexus/plugin/backendclient/service/worklog/status/location/WorklogLocationServiceClient.kt
@@ -1,11 +1,25 @@
 package lt.boldadmin.nexus.plugin.backendclient.service.worklog.status.location
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import lt.boldadmin.nexus.api.service.worklog.status.location.WorklogLocationService
 import lt.boldadmin.nexus.api.type.entity.Collaborator
 import lt.boldadmin.nexus.api.type.valueobject.Location
+import lt.boldadmin.nexus.plugin.backendclient.factory.createUri
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 
-class WorklogLocationServiceClient: WorklogLocationService {
+class WorklogLocationServiceClient : WorklogLocationService {
+
     override fun logWork(collaborator: Collaborator, collaboratorLocation: Location) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val request = HttpRequest.newBuilder()
+            .uri(createUri("worklog/status/log-work/location"))
+            .headers("Content-Type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString(
+                    ObjectMapper().writeValueAsString(Pair(collaborator, collaboratorLocation)))
+            )
+            .build()
+
+        HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.discarding())
     }
 }
