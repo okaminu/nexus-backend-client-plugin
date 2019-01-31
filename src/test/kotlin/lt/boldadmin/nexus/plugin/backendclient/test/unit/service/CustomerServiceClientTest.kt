cddb9acf3a15a069fcbb582534d8b4f566299e15
Okaminu <aurimasdgt@gmail.com>
2019-01-31 12:21:30 +0200

    BA-742: unit test for customer service

---
+++ src/test/kotlin/lt/boldadmin/nexus/plugin/backendclient/test/unit/service/CustomerServiceClientTest.kt
@@ -0,0 +1,82 @@
+package lt.boldadmin.nexus.plugin.backendclient.test.unit.service
+
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.verify
+import lt.boldadmin.nexus.api.type.entity.Customer
+import lt.boldadmin.nexus.plugin.backendclient.httpclient.BackendHttpClient
+import lt.boldadmin.nexus.plugin.backendclient.service.CustomerServiceClient
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import kotlin.test.assertSame
+
+@RunWith(MockitoJUnitRunner::class)
+class CustomerServiceClientTest {
+
+    @Mock
+    private lateinit var httpClientSpy: BackendHttpClient
+
+    private lateinit var customerServiceClientSpy: CustomerServiceClient
+
+    @Before
+    fun setUp() {
+        customerServiceClientSpy = CustomerServiceClient(httpClientSpy)
+    }
+
+    @Test
+    fun `Updates attribute`() {
+        val attributeName = "attributeName"
+        val attributeValue = "attributeValue"
+        val customerId = "customerId"
+
+        customerServiceClientSpy.update(customerId, attributeName, attributeValue)
+
+        verify(httpClientSpy).post("/customer/$customerId/attribute/$attributeName/update", attributeValue)
+    }
+
+    @Test
+    fun `Updates order number`() {
+        val orderNumber: Short = 5
+        val customerId = "customerId"
+
+        customerServiceClientSpy.updateOrderNumber(customerId, orderNumber)
+
+        verify(httpClientSpy).post("/customer/$customerId/attribute/order-number/update", orderNumber)
+    }
+
+
+    @Test
+    fun `Creates project with defaults`() {
+        val expectedCustomer = Customer()
+        val userId = "userId"
+        doReturn(expectedCustomer)
+            .`when`(httpClientSpy)
+            .get("/customer/user/$userId/create-with-defaults", Customer::class.java)
+
+        val actualProject = customerServiceClientSpy.createWithDefaults(userId)
+
+        assertSame(expectedCustomer, actualProject)
+    }
+
+    @Test
+    fun `Gets project by id`() {
+        val expectedCustomer = Customer()
+        val customerId = "customerId"
+        doReturn(expectedCustomer).`when`(httpClientSpy).get("/customer/$customerId", Customer::class.java)
+
+        val actualProject = customerServiceClientSpy.getById(customerId)
+
+        assertSame(expectedCustomer, actualProject)
+    }
+
+    @Test
+    fun `Saves customer`() {
+        val customer = Customer()
+
+        customerServiceClientSpy.save(customer)
+
+        verify(httpClientSpy).postJson("/customer/save", customer)
+    }
+}
