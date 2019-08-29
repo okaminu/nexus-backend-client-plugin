d4ffb8893edfb16fc379ed6eefbd55f5494ca8cd
Okaminu <aurimasdgt@gmail.com>
2019-08-29 17:37:12 +0300

    BA-818: replaced collaborator entity with id

--- src/main/kotlin/lt/boldadmin/nexus/plugin/backendclient/service/worklog/status/location/WorklogLocationServiceClient.kt
+++ src/main/kotlin/lt/boldadmin/nexus/plugin/backendclient/service/worklog/status/location/WorklogLocationServiceClient.kt
@@ -1,12 +1,11 @@
 package lt.boldadmin.nexus.plugin.backendclient.service.worklog.status.location
 
 import lt.boldadmin.nexus.api.service.worklog.status.location.WorklogLocationService
-import lt.boldadmin.nexus.api.type.entity.Collaborator
 import lt.boldadmin.nexus.api.type.valueobject.Coordinates
 import lt.boldadmin.nexus.plugin.backendclient.httpclient.BackendHttpClient
 
 class WorklogLocationServiceClient(private val httpClient: BackendHttpClient): WorklogLocationService {
 
-    override fun logWork(collaborator: Collaborator, collaboratorCoordinates: Coordinates) =
-        httpClient.postAsJson("/worklog/status/log-work/location", Pair(collaborator, collaboratorCoordinates))
+    override fun logWork(collaboratorId: String, coordinates: Coordinates) =
+        httpClient.postAsJson("/worklog/status/log-work/location", Pair(collaboratorId, coordinates))
 }
--- src/test/kotlin/lt/boldadmin/nexus/plugin/backendclient/test/unit/service/worklog/status/location/WorklogLocationServiceClientTest.kt
+++ src/test/kotlin/lt/boldadmin/nexus/plugin/backendclient/test/unit/service/worklog/status/location/WorklogLocationServiceClientTest.kt
@@ -2,7 +2,6 @@ package lt.boldadmin.nexus.plugin.backendclient.test.unit.service.worklog.status
 
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
-import lt.boldadmin.nexus.api.type.entity.Collaborator
 import lt.boldadmin.nexus.api.type.valueobject.Coordinates
 import lt.boldadmin.nexus.plugin.backendclient.httpclient.BackendHttpClient
 import lt.boldadmin.nexus.plugin.backendclient.service.worklog.status.location.WorklogLocationServiceClient
@@ -13,11 +12,11 @@ class WorklogLocationServiceClientTest {
     @Test
     fun `Logs work by location`() {
         val httpClientSpy: BackendHttpClient = mock()
-        val collaborator = Collaborator()
+        val collaboratorId = "collabId"
         val coordinates = Coordinates(123.0, 123.0)
 
-        WorklogLocationServiceClient(httpClientSpy).logWork(collaborator, coordinates)
+        WorklogLocationServiceClient(httpClientSpy).logWork(collaboratorId, coordinates)
 
-        verify(httpClientSpy).postAsJson("/worklog/status/log-work/location", Pair(collaborator, coordinates))
+        verify(httpClientSpy).postAsJson("/worklog/status/log-work/location", Pair(collaboratorId, coordinates))
     }
 }
