0a549ce643466ff51b8c83a4ac7d48c4c9c9eee3
vsh1ft <kastytis.ch@gmail.com>
2019-09-27 14:32:34 +0300

    BA-734: added missing acceptance test step, cleanup

--- src/main/kotlin/lt/boldadmin/nexus/plugin/backendclient/service/worklog/status/WorklogOvertimeServiceClient.kt
+++ src/main/kotlin/lt/boldadmin/nexus/plugin/backendclient/service/worklog/status/WorklogOvertimeServiceClient.kt
@@ -1,10 +1,10 @@
 package lt.boldadmin.nexus.plugin.backendclient.service.worklog.status
 
-import lt.boldadmin.nexus.api.service.worklog.status.WorklogsEndService
+import lt.boldadmin.nexus.api.service.worklog.status.WorklogOvertimeService
 import lt.boldadmin.nexus.plugin.backendclient.httpclient.BackendHttpClient
 
 
-class WorklogsEndServiceClient(private val httpClient: BackendHttpClient): WorklogsEndService {
+class WorklogOvertimeServiceClient(private val httpClient: BackendHttpClient): WorklogOvertimeService {
 
     override fun endAllStartedWorkWhereWorkTimeEnded() =
         httpClient.postWithoutBody("/worklog/status/end/all-started-work-on-ended-work-time")
--- src/test/kotlin/lt/boldadmin/nexus/plugin/backendclient/test/unit/service/worklog/status/WorklogOvertimeServiceClientTest.kt
+++ src/test/kotlin/lt/boldadmin/nexus/plugin/backendclient/test/unit/service/worklog/status/WorklogOvertimeServiceClientTest.kt
@@ -2,7 +2,7 @@ package lt.boldadmin.nexus.plugin.backendclient.test.unit.service.worklog.status
 
 import com.nhaarman.mockitokotlin2.verify
 import lt.boldadmin.nexus.plugin.backendclient.httpclient.BackendHttpClient
-import lt.boldadmin.nexus.plugin.backendclient.service.worklog.status.WorklogsEndServiceClient
+import lt.boldadmin.nexus.plugin.backendclient.service.worklog.status.WorklogOvertimeServiceClient
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -10,16 +10,16 @@ import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
 
 @ExtendWith(MockitoExtension::class)
-class WorklogsEndServiceClientTest {
+class WorklogOvertimeServiceClientTest {
 
     @Mock
     private lateinit var httpClientSpy: BackendHttpClient
 
-    private lateinit var serviceClient: WorklogsEndServiceClient
+    private lateinit var serviceClient: WorklogOvertimeServiceClient
 
     @BeforeEach
     fun setUp() {
-        serviceClient = WorklogsEndServiceClient(httpClientSpy)
+        serviceClient = WorklogOvertimeServiceClient(httpClientSpy)
     }
 
     @Test
