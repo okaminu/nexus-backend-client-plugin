2b9b0a2fab96c367c3122e74e70c1ef606cac124
Andrius Degutis <andriusdgt@gmail.com>
2019-12-03 13:48:53 +0200

    BA-995: fixes

--- src/main/kotlin/lt/boldadmin/nexus/plugin/backendclient/service/worklog/WorklogOvertimeServiceClient.kt
+++ src/main/kotlin/lt/boldadmin/nexus/plugin/backendclient/service/worklog/WorklogOvertimeServiceClient.kt
@@ -3,7 +3,6 @@ package lt.boldadmin.nexus.plugin.backendclient.service.worklog
 import lt.boldadmin.nexus.api.service.worklog.WorklogOvertimeService
 import lt.boldadmin.nexus.plugin.backendclient.httpclient.BackendHttpClient
 
-
 class WorklogOvertimeServiceClient(private val httpClient: BackendHttpClient): WorklogOvertimeService {
 
     override fun endOnOvertime() =
