0a23d431f86f34b3159e6ecc915182d7691a6bf4
Andrius Degutis <andriusdgt@gmail.com>
2020-02-03 14:05:14 +0200

    BA-891: naming fixes

--- src/main/kotlin/lt/boldadmin/nexus/plugin/backendclient/service/collaborator/WorkWeekValidatorClient.kt
+++ src/main/kotlin/lt/boldadmin/nexus/plugin/backendclient/service/collaborator/WorkWeekValidatorClient.kt
@@ -1,11 +1,11 @@
 package lt.boldadmin.nexus.plugin.backendclient.service.collaborator
 
-import lt.boldadmin.nexus.api.service.collaborator.WeekValidatorService
+import lt.boldadmin.nexus.api.service.collaborator.WorkWeekValidatorService
 import lt.boldadmin.nexus.api.type.valueobject.Day
 import lt.boldadmin.nexus.plugin.backendclient.httpclient.BackendHttpClient
 import java.util.*
 
-class WeekValidatorClient(private val httpClient: BackendHttpClient): WeekValidatorService {
+class WorkWeekValidatorClient(private val httpClient: BackendHttpClient): WorkWeekValidatorService {
 
     override fun validate(workWeek: SortedSet<Day>) =
         httpClient.postAsJson("/collaborator/work-week/validate", workWeek)
