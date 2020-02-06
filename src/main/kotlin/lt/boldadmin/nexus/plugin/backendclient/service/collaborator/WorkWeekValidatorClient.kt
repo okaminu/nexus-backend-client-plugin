8c7eb4328de99638d0769fb6f695451452e827da
Andrius Degutis <andriusdgt@gmail.com>
2020-02-06 14:10:58 +0200

    BA-891: validation endpoint prototyping, Validator.kt -> Sortable.kt

--- src/main/kotlin/lt/boldadmin/nexus/plugin/backendclient/service/collaborator/WorkWeekValidatorClient.kt
+++ src/main/kotlin/lt/boldadmin/nexus/plugin/backendclient/service/collaborator/WorkWeekValidatorClient.kt
@@ -1,13 +1,16 @@
 package lt.boldadmin.nexus.plugin.backendclient.service.collaborator
 
+import com.fasterxml.jackson.core.type.TypeReference
 import lt.boldadmin.nexus.api.service.collaborator.WorkWeekValidatorService
 import lt.boldadmin.nexus.api.type.valueobject.Day
+import lt.boldadmin.nexus.api.type.valueobject.WeekConstraintViolation
 import lt.boldadmin.nexus.plugin.backendclient.httpclient.BackendHttpClient
 import java.util.*
 
 class WorkWeekValidatorClient(private val httpClient: BackendHttpClient): WorkWeekValidatorService {
 
-    override fun validate(workWeek: SortedSet<Day>) =
-        httpClient.postAsJson("/collaborator/work-week/validate", workWeek)
+    override fun getConstraintViolations(workWeek: SortedSet<Day>) =
+        httpClient.get("work-week/get-constraint-violations", object: TypeReference<Set<WeekConstraintViolation>>() {})
+//        httpClient.get("work-week/get-constraint-violations", workWeek)
 
 }
